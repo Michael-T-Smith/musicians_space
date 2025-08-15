5a5a66f0eedd1a1f9527b0f55eabb464544c1ce9
Michael Smith <Michael.t.smith95@gmail.com>
2025-08-15 15:35:06 -0500

    reRendering image paths.

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
     basePath: "/musicians_space",
     output: "export", 
     reactStrictMode: true,
+    assetPrefix: isProd ? '/<repo-name>/' : '',
+    images: {
+        unoptimized: true
+    }
 };
 
 export default nextConfig;
--- src/app/components/Events/Events.js
+++ src/app/components/Events/Events.js
@@ -1,16 +1,14 @@
 import Image from 'next/image'
 import events from '../../../../public/assets/data/events.json'
-import image1 from '../../../../public/assets/images/Events/2025/Darkoh_Live.jpeg'
-import image2 from '../../../../public/assets/images/Events/2025/Iron_Verse.jpeg'
-import image3 from '../../../../public/assets/images/Events/2025/Darkoh_Skull_Live.jpeg'
 
 export default function Events(){
-    let map = {
-        "Darkoh_Live": image1,
-        "Iron_Verse": image2,
-        "Detroit_Venue": image3,
-    }
+    const prefix = process.env.NODE_ENV === 'production' ? '/<musicians_space>' : '';
 
+    let map = {
+        "Darkoh_Live": `${prefix}/assets/images/Events/2025/Darkoh_Live.jpeg`,
+        "Iron_Verse": `${prefix}/assets/images/Events/2025/Iron_Verse.jpeg`,
+        "Detroit_Venue": `${prefix}/assets/images/Events/2025/Darkoh_Skull_Live.jpeg`,
+    };
     return(
         <div id="events" className="m-2 pr-2 pl-2">
             <div className="flex justify-center">
